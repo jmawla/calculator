77f92c56ae980aef23282faf9ab96ca3cc048e1b
Jamal <jmawla@yahoo.com>
2014-04-15 21:43:10 -0600

    new calculator.js
    to handle calculator operations

---
+++ calculator.js
@@ -0,0 +1,30 @@
+// 'use strict;'
+
+var calculatorState = {
+	currentValue: '',
+	previousValue: '',
+	pendingOperation: ''
+};
+
+window.calculatorApp = {
+	clickDigit: function(digit){
+		var alertMessage = 'you clicked' + digit;
+		alert('you clicked' + digit);
+
+	},
+	clickDecimal: function(){
+
+	},
+	clickOperator: function(op){
+
+	},
+	clickEquals: function(){
+
+	},
+	clickClear: function(){
+
+	},
+	clickClearEntry: function(){
+
+	}
+}

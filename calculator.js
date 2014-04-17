0cd2524365828d5b00af312ec078f28cea8ccb9b
Jamal <jmawla@yahoo.com>
2014-04-17 11:25:45 -0600

    adding more functionality

--- calculator.js
+++ calculator.js
@@ -7,8 +7,6 @@ var calculatorState = {
 };
 
 var decimalOn = false;
-var newOperation = false;
-
 
 window.calculatorApp = {
 
@@ -20,31 +18,71 @@ window.calculatorApp = {
 	},
 
 	clickOperator: function(op){
-		if(!calculatorState.pendingOperation){
+		if(!calculatorState.pendingOperation && calculatorState.previousValue === ''){
 			calculatorState.pendingOperation = op;
 			calculatorState.previousValue = calculatorState.currentValue;
 			calculatorState.currentValue = '';
+			decimalOn = false;
 		}
 		else{
-			this.clickEquals();
+			calculatorState.pendingOperation = op;
+			this.executeOp();
+//			this.resetall();
+
+		calculatorState.currentValue = '';
+//		calculatorState.pendingOperation = '';
+		decimalOn = false;
 		}
 	},
 
 	clickEquals: function(){
-		if(calculatorState.pendingOperation === '+'){
-			var result = parseInt(calculatorState.previousValue) + parseInt(calculatorState.currentValue);
-			display.value = result;
-			calculatorState.currentValue = '';
-			calculatorState.pendingOperation = '';
+//		calculatorState.pendingOperation = op;
+		this.executeOp();
+		calculatorState.previousValue = '';
+		this.resetall();
+	},
 
-
+	executeOp: function(){
+		var previous = parseFloat(calculatorState.previousValue);
+		var current = parseFloat(calculatorState.currentValue);
+		if(calculatorState.previousValue === ''){
+			display.value = 'ERROR';
 		}
+		else{
+			switch(calculatorState.pendingOperation){
+				case '+':
+					calculatorState.previousValue = previous + current;
+					break;
+				case '-':
+					calculatorState.previousValue = previous - current;
+					break;
+				case '*':
+					calculatorState.previousValue = previous * current;
+					break;
+				case '/':
+					calculatorState.previousValue = previous / current;
+					break;
+			}
+		display.value = calculatorState.previousValue;
+		}
+	},
+
+	resetall: function(){
+		calculatorState.currentValue = '';
+		calculatorState.previousValue = '';
+		calculatorState.pendingOperation = '';
+		decimalOn = false;
+	
+
 	},
 
 	clickDecimal: function(digit){
 		if(!decimalOn){
-			if(calculatorState.currentValue.substr(-1) !== '.'){
+			if( (calculatorState.currentValue.substr(-1) !== '.') && (calculatorState.currentValue !== '') ){
 				calculatorState.currentValue += digit;
+			}
+			else{
+				calculatorState.currentValue = '0' + digit;
 			}
 			display.value = calculatorState.currentValue;
 			decimalOn = true;			
@@ -56,11 +94,12 @@ window.calculatorApp = {
 		display.value = '0';
 		calculatorState.currentValue = '';
 		calculatorState.pendingOperation = '';
+		decimalOn = false;
 	},
 
 	clickClearEntry: function(){
 		calculatorState.currentValue = '';
 		display.value = '0';
-
+		decimalOn = false;
 	}
 }

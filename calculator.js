e1bfca2b679bb04c5267c00d68b11862ef829a7d
Jamal <jmawla@yahoo.com>
2014-04-16 18:06:23 -0600

    added more functionality to calculator.js

--- calculator.js
+++ calculator.js
@@ -6,25 +6,61 @@ var calculatorState = {
 	pendingOperation: ''
 };
 
+var decimalOn = false;
+var newOperation = false;
+
+
 window.calculatorApp = {
+
 	clickDigit: function(digit){
-		var alertMessage = 'you clicked' + digit;
-		alert('you clicked' + digit);
-
+		if(digit !== 0 || calculatorState.currentValue) {
+			calculatorState.currentValue += digit;
+		}
+			display.value = calculatorState.currentValue;
 	},
-	clickDecimal: function(){
 
-	},
 	clickOperator: function(op){
-
+		if(!calculatorState.pendingOperation){
+			calculatorState.pendingOperation = op;
+			calculatorState.previousValue = calculatorState.currentValue;
+			calculatorState.currentValue = '';
+		}
+		else{
+			this.clickEquals();
+		}
 	},
+
 	clickEquals: function(){
+		if(calculatorState.pendingOperation === '+'){
+			var result = parseInt(calculatorState.previousValue) + parseInt(calculatorState.currentValue);
+			display.value = result;
+			calculatorState.currentValue = '';
+			calculatorState.pendingOperation = '';
+
+
+		}
+	},
+
+	clickDecimal: function(digit){
+		if(!decimalOn){
+			if(calculatorState.currentValue.substr(-1) !== '.'){
+				calculatorState.currentValue += digit;
+			}
+			display.value = calculatorState.currentValue;
+			decimalOn = true;			
+		}
 
 	},
+
 	clickClear: function(){
-
+		display.value = '0';
+		calculatorState.currentValue = '';
+		calculatorState.pendingOperation = '';
 	},
+
 	clickClearEntry: function(){
+		calculatorState.currentValue = '';
+		display.value = '0';
 
 	}
 }
